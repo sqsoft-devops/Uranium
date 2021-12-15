ccf57f59c3ef987a73609c7df71d5c4b3ff63c5f
Jelle Spijker <spijker.jelle@gmail.com>
2021-12-15 08:50:45 +0100

    use Alpha version of Arcus

--- conanfile.py
+++ conanfile.py
@@ -28,7 +28,7 @@ class UraniumConan(ConanFile):
 
     def requirements(self):
         self.requires(f"Python/3.8.10@python/stable")
-        self.requires(f"arcus/5.0.0@ultimaker/testing")
+        self.requires(f"arcus/5.0.0-a@ultimaker/testing")
 
     def package(self):
         self.copy("*", src = os.path.join(self.source_folder, "plugins"), dst = os.path.join("site-packages", "plugins"))
